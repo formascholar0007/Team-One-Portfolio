b23231f17544e80f6329abd784fa8ab68a5ce31f
Rahulsharma862 <rs9725240@gmail.com>
2024-06-26 12:31:13 +0530

    Updated Home

--- Portfolio/src/pages/About.jsx
+++ Portfolio/src/pages/About.jsx
@@ -2,7 +2,7 @@
 function About() {
   return (
     <div>
-      <h1 className="text-3xl min-h-screen font-bold min-h-screen">About Us</h1>
+      <h1 className="text-3xl  font-bold min-h-screen">About Us</h1>
       <p>Learn more about us.</p>
     </div>
   );
